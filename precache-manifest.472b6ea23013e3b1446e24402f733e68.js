8ddfc3fccb4d3377bf2b47a400a003c3096dd0a7
Luke Swygart <luke.swygart@CCP.local>
2020-02-17 11:59:02 -0500

    Updates

---
+++ precache-manifest.472b6ea23013e3b1446e24402f733e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8716d8db3dacfd183537a4c036d58ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4161e3643f9cddb15142",
+    "url": "/monsters-rolodex/static/css/main.b95e9508.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4161e3643f9cddb15142",
+    "url": "/monsters-rolodex/static/js/main.63ee2f02.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
